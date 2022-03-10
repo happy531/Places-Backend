71826cadc3f777e1c9eee071ecc164ff0419d97e
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-10 21:39:10 +0100

    add get place by id route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Warsaw',
+    description: 'One of the highest buildings in Warsaw!',
+    location: {
+      lat: 52.231838,
+      lng: 21.005995
+    },
+    address: 'plac Defilad 1, 00-901 Warszawa',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place});
 });
 
 module.exports = router;
