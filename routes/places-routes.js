9dc9aa96b2ed5aac2d73eaad5b28387ac5135a9e
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-10 21:41:18 +0100

    add get places by user id route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -5,23 +5,35 @@ const router = express.Router();
 const DUMMY_PLACES = [
   {
     id: 'p1',
-    title: 'Warsaw',
-    description: 'One of the highest buildings in Warsaw!',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     location: {
-      lat: 52.231838,
-      lng: 21.005995
+      lat: 40.7484474,
+      lng: -73.9871516
     },
-    address: 'plac Defilad 1, 00-901 Warszawa',
+    address: '20 W 34th St, New York, NY 10001',
     creator: 'u1'
   }
 ];
 
 router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
+  const placeId = req.params.pid; // { pid: 'p1' }
+
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
-  res.json({place});
+
+  res.json({ place }); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  res.json({ place });
 });
 
 module.exports = router;
