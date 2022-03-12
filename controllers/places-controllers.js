143b9aeb19be64a1375b1002971383a0b27e4dbc
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-12 09:29:38 +0100

    add users routes

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -36,7 +36,7 @@ const getPlaceById = (req, res, next) => {
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const places = DUMMY_PLACES.find(p => {
+  const places = DUMMY_PLACES.filter(p => {
     return p.creator === userId;
   });
 
@@ -46,7 +46,7 @@ const getPlacesByUserId = (req, res, next) => {
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -87,7 +87,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlacesByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
