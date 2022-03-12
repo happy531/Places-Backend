076555bd84bd7147ab6038fe44abbf03661b9d4c
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-12 17:13:13 +0100

    connect MongoDB Atlas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -25,4 +27,11 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+mongoose
+    .connect(`mongodb+srv://happy531:${process.env.MONGO_DB_PASS}@cluster0.azutq.mongodb.net/places?retryWrites=true&w=majority`)
+    .then(() => {
+      app.listen(5000);
+    })
+    .catch(err => {
+      console.log(err);
+    })
