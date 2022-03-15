c846315d1946663a5029e5636411f140b7d3fe0e
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-15 14:58:19 +0100

    change status codes in some http error handlers

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     return next();
   }
   try {
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    const error = new HttpError('Authentication failed!', 401);
+    const error = new HttpError('Authentication failed!', 403);
     return next(error);
   }
 };
