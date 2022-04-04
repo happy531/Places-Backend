8077ff0c3fae2d4c3d80a9b2df860e35d0b05dea
happy531 <jedrzejwesolowski177@gmail.com>
2022-04-04 13:42:25 +0200

    place address in now not required as well as user profile image

--- models/place.js
+++ models/place.js
@@ -6,10 +6,10 @@ const placeSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  address: { type: String, required: true },
+  address: { type: String, required: false },
   location: {
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true }
+    lat: { type: Number, required: false },
+    lng: { type: Number, required: false }
   },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
 });
