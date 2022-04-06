3e19c9a76779f79d37bad2c3aea877ed03d91ce1
Jędrzej Wesołowski <71727233+happy531@users.noreply.github.com>
2022-04-06 12:44:18 +0200

    images are now required

--- models/place.js
+++ models/place.js
@@ -6,10 +6,10 @@ const placeSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  address: { type: String, required: false },
+  address: { type: String, required: true },
   location: {
-    lat: { type: Number, required: false },
-    lng: { type: Number, required: false }
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true }
   },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
 });
